a91683567a8c1e9e0a3fa7241a365c36a69d5e90
RH <reganhwang@protonmail.com>
2024-10-26 15:37:34 +0100

    ..

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)   # Lets Next JS app make requests to the server
@@ -7,9 +7,14 @@ CORS(app)   # Lets Next JS app make requests to the server
 def hello():
     return jsonify(message="Hello world!")
 
-@app.route('/api/dx')
-def diagnosis():
-    return
+@app.route('/api/dx/send_text', methods =['POST'])
+def dx_test():
+    return jsonify(message="Text received.")
+
+
+@app.route('/api/dx/send_picture', methods = ['POST'])
+def dx_picture():
+    return jsonify(message="Photo received.")
 
 @app.route('/api/translation')
 def translation():
